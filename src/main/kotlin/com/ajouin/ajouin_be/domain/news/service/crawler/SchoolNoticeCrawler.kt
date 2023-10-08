31ddd948060cc3396aa0152eda8d22bbd88fe6a7
moon <88218891+man-moon@users.noreply.github.com>
2023-10-08 19:47:58 +0900

    [새로운 기능] 슬랙알림

--- src/main/kotlin/com/ajouin/ajouin_be/domain/news/service/crawler/SchoolNoticeCrawler.kt
+++ src/main/kotlin/com/ajouin/ajouin_be/domain/news/service/crawler/SchoolNoticeCrawler.kt
@@ -5,8 +5,10 @@ import com.ajouin.ajouin_be.domain.news.domain.SchoolNotice
 import com.ajouin.ajouin_be.domain.news.domain.Type
 import com.ajouin.ajouin_be.domain.news.repository.LastIdPerTypeRepository
 import com.ajouin.ajouin_be.domain.news.repository.SchoolNoticeRepository
+import com.slack.api.Slack
 import org.jsoup.Jsoup
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -20,12 +22,18 @@ class SchoolNoticeCrawler (
 
     private val logger = LoggerFactory.getLogger(SchoolNoticeCrawler::class.java)
 
+    @Value("\${slack.webhook.url}")
+    lateinit var webHookUrl: String
     @Scheduled(fixedRate = 600000)  // 10분마다
     @Transactional
     fun crawl() {
         for (strategy in strategies) {
             crawlByStrategy(strategy)
         }
+        logger.info("공지사항 데이터 수집 완료")
+
+        val slackClient = Slack.getInstance()
+        slackClient.send(webHookUrl, "{\"text\" : \"공지사항 데이터 수집 완료\"}")
     }
 
     fun crawlByStrategy(strategy: NoticeCrawlerStrategy) {
